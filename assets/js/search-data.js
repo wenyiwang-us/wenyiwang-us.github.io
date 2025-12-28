ee52c1bd07167adfea75b3a0f1e7bc2184ae2a43
wenyiwang-us <91695436+wenyiwang-us@users.noreply.github.com>
2025-12-28 20:46:59 +0000

    Deploying to gh-pages from @ wenyiwang-us/wenyiwang-us.github.io@15f97276b9ad908e8ff35ede8e95af80b6b23fe1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -437,7 +437,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/wenyiw-cv.pdf", "_blank");
         },
       },{
         id: 'social-email',
